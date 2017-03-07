a1a942a3c674a8a33602d8ad7dce4c78ed25e7d9
maitanerm <maivask@gmail.com>
2017-03-07 18:49:02 +0100

    Update Jugador.java

--- Jugador.java
+++ Jugador.java
@@ -37,5 +37,27 @@ public class Jugador extends Usuario {
 		coor.crearCoordenada(f, c);
 		return coor;		
 	}
+	
+	package packModelo;
+public class Ordenador extends Usuario {
+
+	private static Ordenador miOrdenador;
+
+	private Ordenador() {
+		super();
+	}
+	public static Ordenador getOrdenador() {
+		if(miOrdenador==null){
+			miOrdenador= new Ordenador();
+		}
+		return miOrdenador;
+	}
+	public void ponerEscudo() {
+		Escudo miEscudo = delEscudo();
+	}
+	private Escudo delEscudo() {
+		return null;
+	}
+}
 
 }
