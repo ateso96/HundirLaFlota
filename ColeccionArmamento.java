ea6e84bbd807f9bfd1a9775c325922ba17dc1ec7
maitanerm <maivask@gmail.com>
2017-03-07 19:05:00 +0100

    Update ColeccionArmamento.java

--- ColeccionArmamento.java
+++ ColeccionArmamento.java
@@ -7,28 +7,18 @@ public class ColeccionArmamento {
 	private Collection<Armamento> col;
 
 	public ColeccionArmamento() {
-		// TODO - implement ColeccionArmamento.ColeccionArmamento
-		throw new UnsupportedOperationException();
 	}
 
 	private Iterator<Armamento> getIterador() {
-		// TODO - implement ColeccionArmamento.getIterador
-		throw new UnsupportedOperationException();
 	}
 
 	public void delArmas() {
-		// TODO - implement ColeccionArmamento.delArmas
-		throw new UnsupportedOperationException();
 	}
 
 	public void addArmamento() {
-		// TODO - implement ColeccionArmamento.addArmamento
-		throw new UnsupportedOperationException();
 	}
 
 	public Escudo delEscudo() {
-		// TODO - implement ColeccionArmamento.delEscudo
-		throw new UnsupportedOperationException();
 	}
 
 }
