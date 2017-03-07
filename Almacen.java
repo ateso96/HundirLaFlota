b6f3331545929e984aa2ee6c28e154b3dad29ca6
maitanerm <maivask@gmail.com>
2017-03-07 19:00:17 +0100

    Update Almacen.java

--- Almacen.java
+++ Almacen.java
@@ -7,32 +7,18 @@ public class Almacen {
 	private static Almacen miAlmacen;
 
 	private Almacen() {
-		// TODO - implement Almacen.Almacen
-		throw new UnsupportedOperationException();
 	}
 
 	public static Almacen getAlmacen() {
-		// TODO - implement Almacen.getAlmacen
-		throw new UnsupportedOperationException();
 	}
 
 	public Armamento comprar() {
-		// TODO - implement Almacen.comprar
-		throw new UnsupportedOperationException();
 	}
-
-	/**
-	 * 
-	 * @param pArmamento
-	 */
+	
 	private void reducirExistencias(Armamento pArmamento) {
-		// TODO - implement Almacen.reducirExistencias
-		throw new UnsupportedOperationException();
 	}
 
 	public void inicializarAlmacen() {
-		// TODO - implement Almacen.inicializarAlmacen
-		throw new UnsupportedOperationException();
 	}
 
 }
