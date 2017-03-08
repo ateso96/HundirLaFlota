918c2873bd1f572bf1ab4f222ad77db2744a9eea
ateso96 <andoniteso@gmail.com>
2017-03-08 18:16:38 +0100

    Update Fragata.java

--- Fragata.java
+++ Fragata.java
@@ -1,22 +1,10 @@
 package packModelo;
 public class Fragata extends Barco {
 
-	/**
-	 * @param pEstado
-	 * @param pOrientacion
-	 * @param pEscudo
-	 * @param pCoordInicio
-	 * @param pCoordFin
-	 * @param pLongitud
-	 */
-	public Fragata(String pEstado, char pOrientacion, Escudo pEscudo, Coordenada pCoordInicio, Coordenada pCoordFin,
-			int pLongitud) {
-		super(pEstado, pOrientacion, pEscudo, pCoordInicio, pCoordFin, pLongitud);
-		// TODO Auto-generated constructor stub
-	}
-
+	private static int longitud = 1;
 	
-
-
+	public Fragata() {
+		super(longitud);
+	}
 
 }
