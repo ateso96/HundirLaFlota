b007ad545f97b96c0365f409c040a5328513a4e7
ateso96 <andoniteso@gmail.com>
2017-03-08 18:34:52 +0100

    Update Armamento.java

--- Armamento.java
+++ Armamento.java
@@ -3,9 +3,8 @@ public abstract class Armamento {
 
 	private int precio;
 
-	public Armamento() {
-		// TODO - implement Armamento.Armamento
-		throw new UnsupportedOperationException();
+	public Armamento(int pPrecio) {
+		precio = pPrecio;
 	}
 
 }
