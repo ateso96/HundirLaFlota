660e19b84ac989750302ef15bea6d582ec173b55
ateso96 <andoniteso@gmail.com>
2017-03-08 18:15:32 +0100

    Update Portaaviones.java

--- Portaaviones.java
+++ Portaaviones.java
@@ -1,22 +1,10 @@
 package packModelo;
 public class Portaaviones extends Barco {
 
-	/**
-	 * @param pEstado
-	 * @param pOrientacion
-	 * @param pEscudo
-	 * @param pCoordInicio
-	 * @param pCoordFin
-	 * @param pLongitud
-	 */
-	public Portaaviones(String pEstado, char pOrientacion, Escudo pEscudo, Coordenada pCoordInicio,
-			Coordenada pCoordFin, int pLongitud) {
-		super(pEstado, pOrientacion, pEscudo, pCoordInicio, pCoordFin, pLongitud);
-		// TODO Auto-generated constructor stub
-	}
-
+	private static int longitud = 4;
 	
-
-
+	public Portaaviones() {
+		super(longitud);
+	}
 
 }
