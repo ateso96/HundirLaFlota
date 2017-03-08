64da1c0f6504c4a778f1abc425c5dbda276d2d13
ateso96 <andoniteso@gmail.com>
2017-03-08 18:14:14 +0100

    Update Submarino.java

--- Submarino.java
+++ Submarino.java
@@ -1,22 +1,10 @@
 package packModelo;
 public class Submarino extends Barco {
 
-	/**
-	 * @param pEstado
-	 * @param pOrientacion
-	 * @param pEscudo
-	 * @param pCoordInicio
-	 * @param pCoordFin
-	 * @param pLongitud
-	 */
-	public Submarino(String pEstado, char pOrientacion, Escudo pEscudo, Coordenada pCoordInicio, Coordenada pCoordFin,
-			int pLongitud) {
-		super(pEstado, pOrientacion, pEscudo, pCoordInicio, pCoordFin, pLongitud);
-		// TODO Auto-generated constructor stub
+	private static int longitud = 3;
+	
+	public Submarino () {
+		super(longitud);
 	}
 
-	
-
-
-	
 }
