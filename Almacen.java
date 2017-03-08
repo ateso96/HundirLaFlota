f24db24886f9a03345ddf7086fa093114fb6bca3
ateso96 <andoniteso@gmail.com>
2017-03-08 13:48:31 +0100

    Update Almacen.java
    Constructora

--- Almacen.java
+++ Almacen.java
@@ -6,10 +6,25 @@ public class Almacen {
 	private Collection<Armamento> existencias;
 	private static Almacen miAlmacen;
 
-	private Almacen() {
+private Almacen() {
+		for (int i = 1; i <= 20; i ++)
+			existencias.add(FactoryArmamento.getFactoryArmamento().crearArma("Bomba"));
+		for (int i = 1; i <= 6; i ++)
+			existencias.add(FactoryArmamento.getFactoryArmamento().crearArma("Misil"));
+		for (int i = 1; i <= 4; i ++)
+			existencias.add(FactoryArmamento.getFactoryArmamento().crearArma("MisilNS"));
+		for (int i = 1; i <= 4; i ++)
+			existencias.add(FactoryArmamento.getFactoryArmamento().crearArma("MisilEO"));
+		for (int i = 1; i <= 2; i ++)
+			existencias.add(FactoryArmamento.getFactoryArmamento().crearArma("MisilBOOM"));
+		for (int i = 1; i <= 2; i ++)
+			existencias.add(FactoryArmamento.getFactoryArmamento().crearArma("Escudo"));
 	}
 
 	public static Almacen getAlmacen() {
+		if (miAlmacen == null)
+			miAlmacen = new Almacen();
+		return miAlmacen;
 	}
 
 	public Armamento comprar() {
