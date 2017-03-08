4c0a60374253a1765ff045f21111860e2da628cb
ateso96 <andoniteso@gmail.com>
2017-03-08 18:17:41 +0100

    Update Destructor.java

--- Destructor.java
+++ Destructor.java
@@ -1,21 +1,10 @@
 package packModelo;
 public class Destructor extends Barco {
 
-	/**
-	 * @param pEstado
-	 * @param pOrientacion
-	 * @param pEscudo
-	 * @param pCoordInicio
-	 * @param pCoordFin
-	 * @param pLongitud
-	 */
-	public Destructor(String pEstado, char pOrientacion, Escudo pEscudo, Coordenada pCoordInicio, Coordenada pCoordFin,
-			int pLongitud) {
-		super(pEstado, pOrientacion, pEscudo, pCoordInicio, pCoordFin, pLongitud);
-		// TODO Auto-generated constructor stub
-	}
-
+	private static int longitud = 2; 
 	
-
+	public Destructor() {
+		super(longitud);
+	}
 
 }
