808f821d3ffe66036c37acb27ecc4c0ae882859c
ateso96 <andoniteso@gmail.com>
2017-03-08 18:55:35 +0100

    Update Misil.java

--- Misil.java
+++ Misil.java
@@ -1,9 +1,11 @@
 package packModelo;
 public class Misil extends Arma {
 
+	private static int precio = 30;
+	private static char danoZona;
+
 	public Misil() {
-		// TODO - implement Misil.Misil
-		throw new UnsupportedOperationException();
+		super(precio, danoZona);
 	}
 
 }
